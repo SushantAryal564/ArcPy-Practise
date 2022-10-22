58b482b7bc58d53469d8889759f086d572ebd484
sushant <aryalsushant564@gmail.com>
2022-10-22 14:42:56 +0545

    Python script to count number of feature in and within buffer of another feature

--- intro.py
+++ intro.py
@@ -2,7 +2,14 @@ import arcpy
 
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = r"D:\Sushant Aryal\Programming\ArcPy\arcgis Files\TestProject.gdb"
-fc = r"D:\Sushant Aryal\Programming\ArcPy\data\countries\ne_10m_admin_0_countries.shp"
+
+def print_message(message):
+    arcpy.AddMessage(message)
+    print(message)
+
+fc= arcpy.GetParameterAsText(0)
+if(fc==""):
+    fc = r"D:\Sushant Aryal\Programming\ArcPy\data\countries\ne_10m_admin_0_countries.shp"
 numFeature= arcpy.management.GetCount(fc)
-arcpy.analysis.Select(fc,"Egypt","NAME='Egypt'")
-print ("shapefile has a",numFeature,"features")
+#arcpy.analysis.Select(fc,"Egypt","NAME='Egypt'")
+print_message("shapefile has a {} features".format(numFeature))
---
+++ selectCountrywithin200km.py
@@ -0,0 +1,11 @@
+﻿import arcpy
+arcpy.env.overwriteOutput = True
+with arcpy.EnvManager(scratchWorkspace=r"D:\Sushant Aryal\Programming\ArcPy\arcgis Files\TestProject.gdb", workspace=r"D:\Sushant Aryal\Programming\ArcPy\arcgis Files\TestProject.gdb"):
+    country=arcpy.GetParameterAsText(0)
+    countries = r"D:\Sushant Aryal\Programming\ArcPy\data\countries\ne_10m_admin_0_countries.shp"
+    places = r"D:\Sushant Aryal\Programming\ArcPy\data\populatedPlaces\ne_10m_populated_places.shp"
+    arcpy.analysis.Select(countries, r"D:\Sushant Aryal\Programming\ArcPy\arcgis Files\TestProject.gdb\selCountry", "NAME = '{}'".format(country))
+    arcpy.analysis.Buffer("selCountry", r"D:\Sushant Aryal\Programming\ArcPy\arcgis Files\TestProject.gdb\selCountry_Buffer", "200 Kilometers", "FULL", "ROUND", "NONE", None, "PLANAR")
+    arcpy.analysis.Clip(places, "selCountry_Buffer", r"D:\Sushant Aryal\Programming\ArcPy\arcgis Files\TestProject.gdb\Places_Clip", None)
+    places = arcpy.management.GetCount("Places_Clip")
+    arcpy.AddMessage("Total places in and around {0} 200Km buffer is {1} ".format(country,places))
